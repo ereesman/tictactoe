749a6656d5e2e5d6a9f7328c382dc15540245620
Eddie Reesman <eddiereesman@gmail.com>
2017-07-17 08:47:46 -0400

    basic game working with curses

--- tictactoe.py
+++ tictactoe.py
@@ -1,84 +1,130 @@
-"""
+import time
+import curses
+from curses import wrapper
 
+"""
 A game of tic tac toe
-
 """
 
-turn = 0
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+turn = 1
 
-def print_board():
+def print_board(stdscr):
+    global board
 
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + ' \n' +
-          '-----------\n' +
-          ' ' + board[4] + ' | ' + board[5] + ' | ' + board[6] + ' \n' +
-          '-----------\n' +
-          ' ' + board[7] + ' | ' + board[8] + ' | ' + board[9] + ' \n')
+    stdscr.addstr(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + ' \n' +
+                  '-----------\n' +
+                  ' ' + board[4] + ' | ' + board[5] + ' | ' + board[6] + ' \n' +
+                  '-----------\n' +
+                  ' ' + board[7] + ' | ' + board[8] + ' | ' + board[9] + ' \n')
 
 def is_tic_tac_toe(player):
+    global board
 
-    # top to bottom check
-    if (board[1] == player and board[2] == player and board[3] == player):
-        return True
-    elif (board[4] == player and board[5] == player and board[6] == player):
-        return True
-    elif (board[7] == player and board[8] == player and board[9] == player):
-        return True
     # left to right check
-    if (board[1] == player and board[4] == player and board[7] == player):
+    if board[1] == player and board[2] == player and board[3] == player:
         return True
-    elif (board[2] == player and board[5] == player and board[8] == player):
+    elif board[4] == player and board[5] == player and board[6] == player:
         return True
-    elif (board[3] == player and board[6] == player and board[9] == player):
+    elif board[7] == player and board[8] == player and board[9] == player:
+        return True
+    # top to bottom check
+    if board[1] == player and board[4] == player and board[7] == player:
+        return True
+    elif board[2] == player and board[5] == player and board[8] == player:
+        return True
+    elif board[3] == player and board[6] == player and board[9] == player:
         return True
     # diagonal check
-    if (board[1] == player and board[5] == player and board[9] == player):
+    if board[1] == player and board[5] == player and board[9] == player:
         return True
-    elif (board[3] == player and board[5] == player and board[7] == player):
+    elif board[3] == player and board[5] == player and board[7] == player:
         return True
     else:
         return False
 
-def is_game_over():
+def is_game_over(stdscr):
+    global board
+    global turn
 
-    if turn >= 8:
-        return True
+    stdscr.clear()
     if is_tic_tac_toe('x'):
-        print_board()
-        print('Game over! x wins!')
+        stdscr.addstr('Game over! x wins!\n\n')
+        print_board(stdscr)
+        stdscr.refresh()
         return True
-    if is_tic_tac_toe('o'):
-        print_board()
-        print('Game over! o wins!')
+    elif is_tic_tac_toe('o'):
+        stdscr.addstr('Game over! o wins!\n\n')
+        print_board(stdscr)
+        stdscr.refresh()
+        return True
+    elif turn >= 10:
+        stdscr.addstr('Game over! Draw!\n\n')
+        print_board(stdscr)
+        stdscr.refresh()
         return True
     else:
         return False
 
-def game_loop():
-    
+def print_board_pos_occupied(stdscr):
+    global board
+    global turn
+
+    stdscr.clear()
+    stdscr.addstr('Turn: ' + str(turn) + '\n\n')
+    print_board(stdscr)
+    stdscr.addstr('\nBoard position is occupied!')
+    stdscr.refresh()
+    time.sleep(1)
+
+def print_not_a_number(stdscr):
+    global board
+    global turn
+
+    stdscr.clear()
+    stdscr.addstr('Turn: ' + str(turn) + '\n\n')
+    print_board(stdscr)
+    stdscr.addstr('\noops, you didn\'t enter a number!')
+    stdscr.refresh()
+    time.sleep(1)
+
+def game_loop(stdscr):
     global board
     global turn
     
-    # while game is not over, take user input by location
-    while not is_game_over():
-        print('Turn: {}').format(turn)
-        print_board()
+    # inside game loop
+    while not is_game_over(stdscr):
 
-    # update board at location with x if (turn % 2 == 0), else o
-        if (turn % 2 == 0):
-            move = input('Player x turn, enter a board position 1...9: ')
-            if (board[move] == ' '):
-                board[move] = 'x'
-                turn += 1
-            else:
-                print('Board position is occupied!')
+        stdscr.clear()
+        stdscr.addstr('Turn: ' + str(turn) + '\n\n')
+        print_board(stdscr)
+        stdscr.refresh()
+
+        if turn % 2 is not 0:
+            stdscr.addstr('\nPlayer x turn, enter a board position 1...9: ')
+            c = stdscr.getch()
+            try:
+                if board[int(chr(c))] == ' ':
+                    board[int(chr(c))] = 'x'
+                    turn += 1
+                else:
+                    print_board_pos_occupied(stdscr)
+            except ValueError:
+                print_not_a_number(stdscr)
         else:
-            move = input('Player o turn, enter a board position 1...9: ')
-            if (board[move] == ' '):
-                board[move] = 'o'
-                turn += 1
-            else:
-                print('Board position is occupied!')
+            stdscr.addstr('\nPlayer o turn, enter a board position 1...9: ')
+            stdscr.refresh()
+            c = stdscr.getch()
+            try:
+                if board[int(chr(c))] == ' ':
+                    board[int(chr(c))] = 'o'
+                    turn += 1
+                else:
+                    print_board_pos_occupied(stdscr)
+            except ValueError:
+                print_not_a_number(stdscr)
+
+    input('\nCTRL + C TO EXIT')
 
 if __name__ == '__main__':
-    game_loop()
+    wrapper(game_loop)
